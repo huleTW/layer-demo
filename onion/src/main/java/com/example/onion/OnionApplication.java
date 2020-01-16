9e43c3cc8946cf92af6d464fcebf26f771f6f832
Hu Le <le.hu.2010@163.com>
2020-01-16 19:52:04 +0800

    add onion

--- onion/src/main/java/com/example/onion/OnionApplication.java
+++ onion/src/main/java/com/example/onion/OnionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnionApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OnionApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OnionApplication.class, args);
+	}
 
 }
